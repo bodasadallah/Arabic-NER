7c98aafab50c82ad7e5076d0abaf2aae31fc3331
boda <boda998@yahoo.com>
2021-12-14 13:09:15 +0200

    edit demo file

--- ner.py
+++ ner.py
@@ -1,4 +1,5 @@
 from genericpath import exists
+from numpy.core.fromnumeric import shape
 import pandas as pd
 import numpy as np
 from helpers.download_model  import download_file_from_google_drive
@@ -7,6 +8,7 @@ from helpers.helper import en_to_ar
 import torch
 import os
 import gdown
+from transformers import AutoModel
 MODEL_NAME = 'aubmindlab/bert-base-arabertv02'
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -32,24 +34,23 @@ if not os.path.exists(destination):
     
 
 
+model = torch.load(destination ,map_location='cpu')
+model.eval()
 TOKENIZER = AutoTokenizer.from_pretrained(MODEL_NAME)
-
-
 
 
 label_list = list(pd.read_csv(f'{DIR_PATH}/model/ours/label_list.txt', header=None, index_col=0).T)
 label_map = { v:index for index, v in enumerate(label_list) }
 inv_label_map = {i: label for i, label in enumerate(label_list)}
 
+model.config.id2label = inv_label_map
+model.config.label2id = label_map
 
-model = torch.load(destination ,map_location='cpu')
-model.eval()
+
+
 
 def predict_sent(sentences):
 
-    sentences = sentences.split('\n')
-    for s in sentences:
-        print( 'HERE', s)
     # input_ids  = TOKENIZER.encode(sentences, return_tensors='pt')
     out = TOKENIZER.batch_encode_plus(sentences, return_tensors='pt',padding=True)
     result = ''
@@ -59,33 +60,42 @@ def predict_sent(sentences):
 
     with torch.no_grad():
         model.to('cpu')
-        output = model(input_ids,attention_mask)
-
+        output = model(**out)
     label_indices = np.argmax(output[0].to('cpu').numpy(), axis=2)
 
 
     tokens = [TOKENIZER.convert_ids_to_tokens(x.to('cpu').numpy()) for x in input_ids]
 
     new_tokens, new_labels = [], []
+    all_labels = []
+    all_tokens = []
     for sent_tokens , sent_labels in zip(tokens, label_indices):
+        new_tokens = []
+        new_labels = []
         for token, label_idx  in zip(sent_tokens , sent_labels):
             if token.startswith("##"):
                 new_tokens[-1] = new_tokens[-1] + token[2:]
             elif token in TOKENIZER.all_special_tokens:
                 continue
             else:
-                new_labels.append(inv_label_map[label_idx])
+                new_labels.append(model.config.id2label[label_idx])
                 new_tokens.append(token)
 
+        all_labels.append(new_labels)
+        all_tokens.append(new_tokens)
 
-    for token, label in zip(new_tokens, new_labels):
-        if(label == 'O'):
-            continue
-        print("{}\t{}".format(label, token))
-        s = f"{en_to_ar[label]}     {label}      {token}"
-        result = result + s + '\n'
-    return result, new_labels, new_tokens
+    return all_labels, all_tokens
 
 
 
-print(predict_sent(" النجم محمد صلاح لاعب المنتخب المصري يعيش في مصر بالتحديد من المنصوره"))
+
+
+s = [ 'انا ساكن في حدايق الزتون و بدرس في جامعه عين شمس','النجم محمد صلاح لاعب ليفربول من مواليد قريه نجريج الشرقيه',"يقع بحر الأمازون في قارة أمريكا الجنوبية" ,"انا من عين شمس"]
+
+l, t = predict_sent(s)
+
+for a,b in zip(l,t):
+    for c,d in zip(a,b):
+        print(f'{c}  {d}')
+    print ('*' *25)
+
